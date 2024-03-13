5f0154d5871845b685dce3bb317589039951010e
aay7ush <aay7ush@gmail.com>
2024-03-13 18:50:39 +0530

    add starter files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {
-      colors: {
-        'clr-bg-main': 'var(--clr-bg-main)',
-        'clr-bg-input': 'var(--clr-bg-input)',
-        'clr-bg-border': 'var(--clr-bg-border)',
-        'clr-text-primary': 'var(--clr-text-primary)',
-        'clr-text-secondary': 'var(--clr-text-secondary)',
-        'clr-accent': 'var(--clr-accent)',
-        'clr-line-through': 'var(--clr-line-through)',
-        'clr-gradient-1': 'var(--clr-gradient-1)',
-        'clr-gradient-2': 'var(--clr-gradient-2)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
